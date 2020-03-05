1aa7a5134d8dd6e1c05c703312bb41d8a022b244
Mathias <juniormathiias@gmail.com>
2020-03-05 00:09:10 +0000

    fixing some bugs and add one more function

--- app.js
+++ app.js
@@ -37,31 +37,47 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         roundScore = roundScore + dice
         document.querySelector('#current-'+ activePlayer).textContent= roundScore
     }else{
-        // next player
-     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
-        /* 
-        the same from the top
-        if(activePlayer === 0){
-            activePlayer = 1
-        }else{
-            activePlayer = 0
-        } */
-
-        roundScore = 0 
-        document.getElementById('current-0').textContent = '0'
-        document.getElementById('current-1').textContent = '0'
-
-        document.querySelector('.player-0-panel').classList.toggle('active')
-        document.querySelector('.player-1-panel').classList.toggle('active')
-
-        //document.querySelector('.player-0-panel').classList.remove('active')
-        //document.querySelector('.player-1-panel').classList.add'active')
-
-        document.querySelector('.dice').style.display = 'none'
+    nextPlayer()
     }
 })
 
 document.querySelector('.btn-hold').addEventListener('click' , function(){
     // add current score to global score
+    scores[activePlayer] += roundScore
     // update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
+    // check if player won the game
+    if(score[activePlayer] >= 10){
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner'
+        document.querySelector('.dice').style.display = 'none'
+        document.querySelector('.player-' + activePlayer + '-panel').classList.Add('winner')
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
+    }else{
+        nextPlayer()
+    }
+    // just calling another function to the next player
+   nextPlayer()
 })
+ 
+function nextPlayer(){
+      // next player
+     // activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+      // the same from the top
+      if(activePlayer === 0){
+          activePlayer = 1
+      }else{
+          activePlayer = 0
+      } 
+
+      roundScore = 0 
+      document.getElementById('current-0').textContent = '0'
+      document.getElementById('current-1').textContent = '0'
+
+      document.querySelector('.player-0-panel').classList.toggle('active')
+      document.querySelector('.player-1-panel').classList.toggle('active')
+
+      //document.querySelector('.player-0-panel').classList.remove('active')
+      //document.querySelector('.player-1-panel').classList.add'active')
+
+      document.querySelector('.dice').style.display = 'none'
+}
